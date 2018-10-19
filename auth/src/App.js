1ee71475a9cc47e4f4a937a6b9646b1a3c64f959
Brandon Shoop <brandon.shoop@boats.com>
2018-10-19 13:23:39 -0400

    lecture 60

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        // Initialize Firebase
+        const config = {
+            apiKey: "AIzaSyBuj1ITGTYXNirCcLeam2yPqZ8H8VS9TL0",
+            authDomain: "auth-58ba1.firebaseapp.com",
+            databaseURL: "https://auth-58ba1.firebaseio.com",
+            projectId: "auth-58ba1",
+            storageBucket: "auth-58ba1.appspot.com",
+            messagingSenderId: "2840192363"
+        }
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An app</Text>    
+                <LoginForm />
             </View>
         );
     }
