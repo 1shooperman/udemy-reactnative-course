1c4d55f0c4f7039281cec56d7cd1ce29650722b8
Brandon Shoop <brandon.shoop@boats.com>
2018-10-20 11:32:02 -0400

    lecture 68

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { text: '' }; 
+    state = { 
+        email: '',
+        password: ''
+    }; 
 
     render() {
         return (
@@ -10,11 +13,22 @@ class LoginForm extends Component {
                 <CardSection>
                     <Input
                         label="Email"
-                        value={this.state.text}
-                        onChangeText={text => this.setState({ text })}
+                        autoCorrect={false}
+                        placeholder="user@example.com"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
                     />
                 </CardSection>
-                <CardSection />
+                <CardSection>
+                    <Input
+                        autoCorrect={false}
+                        secureTextEntry
+                        placeholder="password"
+                        label="Password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
                 <CardSection>
                     <Button>
                         Log in
