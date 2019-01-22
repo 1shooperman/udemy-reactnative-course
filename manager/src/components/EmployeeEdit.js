501bbfc8f9837b327d90a3853e887d36d2a3a5c7
Brandon Shoop <brandon.shoop@boats.com>
2019-01-21 22:03:28 -0500

    lesson 155,156: properly save employee.  there's still an issue with navigating to create after editinggit diff

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Button } from './common'; 
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 import EmployeeForm from './EmployeeForm';
 import _ from 'lodash';
 
@@ -14,7 +14,8 @@ class EmployeeEdit extends Component {
 
     onButtonPress() {
         const { name, phone, shift } = this.props;
-        console.log(name, phone, shift);
+        
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
 
     render() {
@@ -37,4 +38,7 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
+export default connect(mapStateToProps, { 
+    employeeUpdate,
+    employeeSave
+})(EmployeeEdit);
